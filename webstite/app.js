0d33839adde268f5843fad95fbb9dbe1e6d22db2
maguro-24 <152110319+maguro-24@users.noreply.github.com>
2025-05-16 02:28:13 -0500

    Add files via upload

---
+++ webstite/app.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector('.window-wrapper');
+function showPixWindow() {
+  wrapper.classList.add('show-pix');
+}
+function showAboutMeWindow() {
+  wrapper.classList.remove('show-pix');
+}
